470a9c76997ab5588ef91259aa65ce3ca3f61120
Thien Nguyen <j.thien1551@gmail.com>
2022-08-10 13:47:38 +0700

    Add Route web and HomeController, connect DB

---
+++ KhareerSmartStudy/src/main/java/fpt/aptech/KSS/Controller/HomeController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fpt.aptech.KSS.Controller;
+
+import fpt.aptech.KSS.Routes.RouteWeb;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author jthie
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/url")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "view.name";
+    }
+
+    @RequestMapping(value = {RouteWeb.index1URL}, method = RequestMethod.GET)
+//    @GetMapping("/login")
+    public String Login(Model model) {
+
+        return "login";
+    }
+
+}
---
+++ KhareerSmartStudy/src/main/java/fpt/aptech/KSS/Routes/RouteWeb.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fpt.aptech.KSS.Routes;
+
+import fpt.aptech.KSS.Controller.*;
+
+/**
+ * @author Admin
+ */
+public class RouteWeb {
+
+    //    Khu vuc new Model
+    HomeController homeController = new HomeController();
+
+
+    //    Khu vuc khai bao Route
+    public final static String AdminHomeURL = "/home/index";
+
+
+    public final static String index1URL = "/login";
+
+
+
+}
