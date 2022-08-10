e29365bfe438d99646af08f7efb73ebbab707a58
Thien Nguyen <j.thien1551@gmail.com>
2022-08-10 12:57:45 +0700

    First commit

---
+++ KhareerSmartStudy/src/main/java/fpt/aptech/KSS/KssApplication.java
@@ -0,0 +1,13 @@
+package fpt.aptech.KSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KssApplication.class, args);
+	}
+
+}
---
+++ KhareerSmartStudy/src/test/java/fpt/aptech/KSS/KssApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.aptech.KSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
